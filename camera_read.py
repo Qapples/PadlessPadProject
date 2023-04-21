f884c1c67e61ec97048e25ab59725e2cf04403bc
qapples <793678@lcps.org>
2023-04-21 18:36:20 -0400

    Check for main in both camera_read.py and nn_model.py

--- camera_read.py
+++ camera_read.py
@@ -4,42 +4,47 @@ import sys
 import cv2
 import os
 
-output_directory = "training_images"
-labels = ['up', 'down', 'left', 'right']
-current_keys_pressed = []
-break_flag = False
 
-if not os.path.exists(output_directory):
-    os.makedirs(output_directory)
+def main():
+    output_directory = "training_images"
+    labels = ['up', 'down', 'left', 'right']
+    current_keys_pressed = []
+    break_flag = False
+
+    if not os.path.exists(output_directory):
+        os.makedirs(output_directory)
+
+    def save_data(frame, img_id):
+        filename = f'{img_id}_{"_".join(current_keys_pressed)}.jpg'
+        filepath = os.path.join(output_directory, filename)
+        cv2.imwrite(filepath, frame)
+
+        print(f"saved to {filepath}")
+
+    cap = cv2.VideoCapture(0)
+    image_count = 0
+
+    while True:
+        if break_flag:
+            break
+
+        ret, frame = cap.read()
+        cv2.imshow('frame', frame)
+
+        for key in labels:
+            if keyboard.is_pressed(key):
+                current_keys_pressed.append(key)
+
+        if len(current_keys_pressed) > 0:
+            save_data(frame, image_count)
+
+        current_keys_pressed.clear()
+        cv2.waitKey(20)
+        image_count += 1
+
+    cap.release()
+    cv2.destroyAllWindows()
 
 
-def save_data(frame, img_id):
-    filename = f'{img_id}_{"_".join(current_keys_pressed)}.jpg'
-    filepath = os.path.join(output_directory, filename)
-    cv2.imwrite(filepath, frame)
-
-    print(f"saved to {filepath}")
-
-cap = cv2.VideoCapture(0)
-image_count = 0
-
-while True:
-    if break_flag:
-        break
-
-    ret, frame = cap.read()
-    cv2.imshow('frame', frame)
-
-    for key in labels:
-        if keyboard.is_pressed(key):
-            current_keys_pressed.append(key)
-
-    if len(current_keys_pressed) > 0:
-        save_data(frame, image_count)
-
-    current_keys_pressed.clear()
-    cv2.waitKey(20)
-    image_count += 1
-
-cap.release()
-cv2.destroyAllWindows()
+if __name__ == "__main__":
+    main()
--- nn_model.py
+++ nn_model.py
@@ -1,40 +1,56 @@
+import os
 import cv2
 import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense, Flatten
 from keras.optimizers import Adam
 
-cap = cv2.VideoCapture('video.mp4')
 
-num_frames = 100
+def main():
+    image_labels = ["up", "down", "left", "right"]
+    img_size = 128 * 128
+    x_images = []
+    y_labels = []
 
-frames = []
+    for filename in os.listdir("training_images"):
+        if not filename.endswith(".jpg"):
+            continue
 
-for i in range(num_frames):
-    ret, frame = cap.read()
-    if not ret:
-        break
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    frame = cv2.resize(frame, (80, 80))
-    frames.append(frame)
+        label_parts = filename.split("_")[0:]
+        labels = []
 
-data = np.array(frames).astype('float32') / 255.0
-data = data.reshape(num_frames, -1)
+        for label in image_labels:
+            labels.append(1 if label in label_parts else 0)
 
-model = Sequential()
-model.add(Dense(128, input_dim=6400, activation='relu'))
-model.add(Dense(64, activation='relu'))
-model.add(Dense(4, activation='sigmoid'))
+        image = cv2.imread(os.path.join("training_images", filename))
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        image = cv2.resize(image, img_size)
 
-model.compile(loss='binary_crossentropy', optimizer=Adam(lr=0.001), metrics=['accuracy'])
+        x_images.append(image)
+        y_labels.append(labels)
 
-x_train = data[:80,:]
-y_train = np.random.randint(2, size=(80, 4))
-x_test = data[80:,:]
-y_test = np.random.randint(2, size=(20, 4))
+    x_images = np.array(x_images).astype('float32') / 255.0
+    # x_images = x_images.reshape(num_frames, -1)
+    y_labels = np.array(y_labels)
 
-model.fit(x_train, y_train, epochs=50, batch_size=10, verbose=2)
+    model = Sequential()
+    model.add(Dense(128, input_dim=6400, activation='relu'))
+    model.add(Dense(64, activation='relu'))
+    model.add(Dense(4, activation='sigmoid'))
 
-loss, accuracy = model.evaluate(x_test, y_test, verbose=0)
-print(f'Test Loss: {loss:.3f}')
-print(f'Test Accuracy: {accuracy:.3f}')
+    model.compile(loss='binary_crossentropy', optimizer=Adam(lr=0.001), metrics=['accuracy'])
+
+    x_train = x_images[:80, :]
+    y_train = np.random.randint(2, size=(80, 4))
+    x_test = x_images[80:, :]
+    y_test = np.random.randint(2, size=(20, 4))
+
+    model.fit(x_train, y_train, epochs=50, batch_size=10, verbose=2)
+
+    loss, accuracy = model.evaluate(x_test, y_test, verbose=0)
+    print(f'Test Loss: {loss:.3f}')
+    print(f'Test Accuracy: {accuracy:.3f}')
+
+
+if __name__ == "__main__":
+    main()
