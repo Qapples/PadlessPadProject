21e31d523236e3fa610e6dcee6996e761c26c693
qapples <793678@lcps.org>
2023-04-21 14:38:02 -0400

    place images in one folder and name the images with labels

--- camera_read.py
+++ camera_read.py
@@ -1,34 +1,59 @@
+import keyboard
+import time
+import sys
 import cv2
 import os
 
+output_directory = "training_images"
 labels = ['up', 'down', 'left', 'right']
+current_keys_pressed = []
+break_flag = False
 
-data_dirs = [os.path.join(os.getcwd(), label) for label in labels]
+if not os.path.exists(output_directory):
+	os.makedir(output_directory)
 
-for dir_path in data_dirs:
-    if not os.path.exists(dir_path):
-        os.makedirs(dir_path)
-
-def save_data(frame, label):
-    data_dir = data_dirs[label]
-    filename = f'{label}_{len(os.listdir(data_dir))}.jpg'
-    filepath = os.path.join(data_dir, filename)
+def save_data(frame, img_id):
+    filename = f'{img_id}_{"_".join(current_keys_pressed)}.jpg'
+    filepath = os.path.join(output_directory, filename)
     cv2.imwrite(filepath, frame)
 
+def on_key_press(key):
+	if key.name == "q":
+		break_flag = True
+	elif key in labels:
+		current_keys_pressed.append(key.name)	
+
+def on_key_release(key):
+	if key.name == "q":
+		break_flag = True
+	elif key in labels:
+		current_keys_pressed.remove(key.name)
+
+keyboard.on_press(on_key_press)
+keyboard.on_release(on_ley_release)
+
 cap = cv2.VideoCapture(0)
+capture_rate_hz = 50 #in hZ
+image_count = 0
 
 while True:
+	if break_flag:
+		break
+
+	start_time = time.time()
+
     ret, frame = cap.read()
     cv2.imshow('frame', frame)
-    key = cv2.waitKey(1) & 0xFF
 
-    if key == ord('q'):
-        break
+	save_data(frame, image_count)
+	
+	iteration_duration = time.time() - start_time
+	remaining_time = (1.0 / capture_rate_hz) - iteration_duration
 
-    for i, label in enumerate(labels):
-        if key == ord(str(i)):
-            save_data(frame, i)
-            print(f'{label} saved')
-
+	if remaining_time > 0:
+		time.sleep(remaining_time)
+	
+	image_count += 1
+	
 cap.release()
 cv2.destroyAllWindows()
