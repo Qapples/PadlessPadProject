be52bc2915d6064f08b1a5a7749f61662aabf4c6
qapples <793678@lcps.org>
2023-04-21 15:38:04 -0400

    Use kebyoard.is_pressed instead of events

--- camera_read.py
+++ camera_read.py
@@ -10,43 +10,36 @@ current_keys_pressed = []
 break_flag = False
 
 if not os.path.exists(output_directory):
-	os.makedirs(output_directory)
+    os.makedirs(output_directory)
+
 
 def save_data(frame, img_id):
     filename = f'{img_id}_{"_".join(current_keys_pressed)}.jpg'
     filepath = os.path.join(output_directory, filename)
     cv2.imwrite(filepath, frame)
 
-def on_key_press(key):
-	if key.name == "q":
-		break_flag = True
-	elif key in labels:
-		current_keys_pressed.append(key.name)	
-
-def on_key_release(key):
-	if key.name == "q":
-		break_flag = True
-	elif key in labels:
-		current_keys_pressed.remove(key.name)
-
-keyboard.on_press(on_key_press)
-keyboard.on_release(on_key_release)
+    print(f"saved to {filepath}")
 
 cap = cv2.VideoCapture(0)
-capture_rate_hz = 50 #in hZ
 image_count = 0
 
 while True:
-	if break_flag:
-		break
+    if break_flag:
+        break
 
-	ret, frame = cap.read()
-	cv2.imshow('frame', frame)
+    ret, frame = cap.read()
+    cv2.imshow('frame', frame)
 
-	save_data(frame, image_count)
-	
-	cv2.waitKey(20)
-	image_count += 1
-	
+    for key in labels:
+        if keyboard.is_pressed(key):
+            current_keys_pressed.append(key)
+
+    if len(current_keys_pressed) > 0:
+        save_data(frame, image_count)
+
+    current_keys_pressed.clear()
+    cv2.waitKey(20)
+    image_count += 1
+
 cap.release()
 cv2.destroyAllWindows()
