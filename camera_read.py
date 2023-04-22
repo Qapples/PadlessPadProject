cbea14155ff774ba606dde20667fcf8044248a4f
qapples <armand.scotty@yahoo.com>
2023-04-21 22:26:51 -0400

    implement no label images in camera_read.py

--- camera_read.py
+++ camera_read.py
@@ -7,7 +7,8 @@ import os
 
 def main():
     output_directory = "training_images"
-    labels = ['up', 'down', 'left', 'right']
+    labels = ["up", "down", "left", "right"]
+    no_label_key = "space"
     current_keys_pressed = []
     break_flag = False
 
@@ -15,7 +16,7 @@ def main():
         os.makedirs(output_directory)
 
     def save_data(frame, img_id):
-        filename = f'{img_id}_{"_".join(current_keys_pressed)}.jpg'
+        filename = f'{img_id}_{"_".join(current_keys_pressed)}.jpg' if len(current_keys_pressed) > 0 else f"{img_id}.jpg"
         filepath = os.path.join(output_directory, filename)
         cv2.imwrite(filepath, frame)
 
@@ -29,13 +30,13 @@ def main():
             break
 
         ret, frame = cap.read()
-        cv2.imshow('frame', frame)
+        cv2.imshow("frame", frame)
 
         for key in labels:
             if keyboard.is_pressed(key):
                 current_keys_pressed.append(key)
 
-        if len(current_keys_pressed) > 0:
+        if len(current_keys_pressed) > 0 or keyboard.is_pressed(no_label_key):
             save_data(frame, image_count)
 
         current_keys_pressed.clear()
